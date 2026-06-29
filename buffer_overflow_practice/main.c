bdb3ae1837f97accaf3d2bdc3486b283e11a3f21
Zabro000 <reinzabrocki@shaw.ca>
2026-06-29 12:55:12 -0600

    Created a winner fucntion so there is something to actually attack

--- buffer_overflow_practice/main.c
+++ buffer_overflow_practice/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int winner(double t);
+
 int main(int argc, char *argv[]){
 	char buf[256];
 	strcpy(buf, argv[1]);
 	printf("%s\n", buf);
 	return 0;
+}
+
+
+int winner(double t)
+{
+	double data = 90;
+	printf("Successful attack");
 }
